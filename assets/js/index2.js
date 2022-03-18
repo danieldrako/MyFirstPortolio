279ffdf9b357e9e08bb98b96063347fb7a3cfd0c
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2022-03-18 16:49:21 -0600

    Add files via upload
    Primera Versión

---
+++ assets/js/index2.js
@@ -0,0 +1,28 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+// const navClose = document.querySelectorAll(".nav-menu-item a");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("nav-menu_visible")) {
+        navToggle.setAttribute("aria-label", "Cerrar menu");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir menu");
+    }
+});
+
+const nav = document.querySelector('.contenedor-navegacion');
+
+window.addEventListener('scroll', function() {
+    nav.classList.toggle('active', window.scrollY > 0)
+})
+
+
+
+document.addEventListener('click', (e) => {
+    if (e.target.closest('.nav-menu-link')) {
+        navMenu.classList.remove("nav-menu_visible")
+    }
+
+})
